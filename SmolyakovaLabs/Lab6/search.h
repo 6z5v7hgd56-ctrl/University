06e6f8bd0fe79ec0ab2e98843551a030d454298b
Ginguare-hub <ginguare@gmail.com>
2026-04-22 23:45:07 +0300

    Lab6 Smolyakova is started? don't think so

---
+++ SmolyakovaLabs/Lab6/search.h
@@ -0,0 +1,17 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+
+struct Record
+{
+    int id;
+    char name[50];
+    int value;
+};
+
+
+#endif
